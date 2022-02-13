dd11b519fb3bb573559ac22961731698fe9803b3
LLKK <97378349+031031KK@users.noreply.github.com>
2022-02-13 17:13:56 +0800

    Add files via upload

---
+++ 01   cpp.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#define N 55
+int main()
+{
+    int n, i;
+    int num[N] = {0,1,2,3,4}; 
+	for (i = 5; i < 55; i++)
+		num[i] = num[i - 1] + num[i - 3];
+    while (cin >> n && n != 0) 
+	{
+        cout << num[n] << endl;
+    }
+    return 0;
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
